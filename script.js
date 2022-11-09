ea7fe77375c21263b839986776a3a3ce6cd56d7c
Ncarmend <carmn@LAPTOP-2FGGQPL4>
2022-11-09 18:21:25 +0100

    second commit

--- script.js
+++ script.js
@@ -1,98 +1,116 @@
-const body = document.body;
-let addToDoButton = document.getElementById('addToDo');
-let addToDoButtonA = document.getElementById('addToDoA');
-let addToDoButtonB = document.getElementById('addToDoB');
-let toDoContainer = document.getElementById('toDoContainer');
-let inputField = document.getElementById('inputField');
+const inputTsk = document.getElementById("new-task");
+const buttonAdd = document.getElementById("bouton");
+const doingTsk = document.getElementById("doingTsk");
+const donaTsk = document.getElementById("doneTsk");
 
-addToDoButton.addEventListener('click', function () {
-    var paragraph = document.createElement('p');
-    paragraph.classList.add('paragraph-styling');
-    paragraph.innerText = inputField.value;
-    toDoContainer.appendChild(paragraph);
-    inputField.value = '';
-    paragraph.addEventListener('click', function () {
-        paragraph.style.textDecoration = 'line-through';
-    })
-    paragraph.addEventListener('dblclick', function () {
-        if (window.confirm('Excluir ' + paragraph.innerText + '?')) {
-            toDoContainer.removeChild(paragraph);
-        }
-    })
-})
-addToDoButtonA.addEventListener('click', function () {
-    let paragraphA = document.createElement('p');
-    paragraphA.classList.add('paragraph-styling');
-    paragraphA.innerText = inputFieldA.value;
-    toDoContainer.appendChild(paragraphA);
-    inputFieldA.value = '';
-    paragraphA.addEventListener('click', function () {
-        paragraphA.style.textDecoration = 'line-through';
-    })
-    paragraphA.addEventListener('dblclick', function () {
-        if (window.confirm('Excluir ' + paragraphA.innerText + '?')) {
-            toDoContainer.removeChild(paragraphA);
-        }
-    })
-})
-addToDoButtonB.addEventListener('click', function () {
-    let paragraphB = document.createElement('p');
-    paragraphB.classList.add('paragraph-styling');
-    paragraphB.innerText = inputFieldB.value;
-    toDoContainer.appendChild(paragraphB);
-    inputFieldB.value = '';
-    paragraphB.addEventListener('click', function () {
-        paragraphB.style.textDecoration = 'line-through';
-    })
-    paragraphB.addEventListener('dblclick', function () {
-        if (window.confirm('Excluir ' + paragraphB.innerText + '?')) {
-            toDoContainer.removeChild(paragraphB);
-        }
-    })
 
-    let debut = new Date()
-    faireQuelqueChosePendantLongtemps()
-    let fin = new Date()
-    let duree = fin.getTime() - debut.getTime() // La durée en millisecondes
-})
 
-const textinpt = document.createElement("input")
-const div = document.createElement("div")
-body.appendChild(div)
-div.appendChild(textinpt)
-textinpt.type = "text";
-textinpt.placeholder = "Name task";
-textinpt.classList.add("paragraph-styling")
+const difDay = () => {
+      
+    let dateI1 = document.getElementById("dateInput1").value;
+    let dateI2 = document.getElementById("dateInput2").value;
 
-const select = document.createElement("select")
-select.classList.add('paragraph-styling')
-body.appendChild(select)
+    let date1 = new Date(dateI1);
+    let date2 = new Date(dateI2);
+ 
+    let time_difference = date2.getTime() - date1.getTime();
 
-const option = document.createElement("option")
-const option1 = document.createElement("option")
-const option2 = document.createElement("option")
-const btnok = document.createElement("button");
-option.value = "todo"
-option.text = "TO DO";
-option1.value = "doing"
-option1.text = "DOING";
-option2.value = "done"
-option2.text = "DONE";
+    let result = time_difference / (1000 * 60 * 60 * 24);
 
-select.appendChild(option)
-select.appendChild(option1)
-select.appendChild(option2)
-btnok.addEventListener('click', function () {
-    const tache = document.querySelector(".nameInput");
-    function selectt() {
-        let select = document.getElementById("select");
-        function selec() {
-            alert(select.selectedOptions[0].value);
-            console.log(selec)
-        }
+    return document.getElementById("result").innerHTML =
+        result + " days to deadline  ";
+}
+
+const createNewTaskElement = (taskString) => {
+    const listItem = document.createElement("li");
+    const checkBox = document.createElement("input"); 
+    const label = document.createElement("label");
+    const editInput = document.createElement("input"); 
+    const insertDue = document.getElementById("result");
+    const editButton = document.createElement("button");
+    const deleteButton = document.createElement("button");
+    checkBox.type = "checkbox";
+    editInput.type = "text";
+    editButton.innerText = "Edit";
+    editButton.className = "edit";
+    deleteButton.innerText = "Delete";
+    deleteButton.className = "delete";
+    insertDue.className = "resultats";
+
+    label.innerText = taskString;
+    listItem.appendChild(checkBox);
+    listItem.appendChild(label);
+    listItem.appendChild(editInput);
+    listItem.appendChild(editButton);
+    listItem.appendChild(deleteButton);
+    label.appendChild(insertDue);
+
+    return listItem;
+}
+
+const addTask = () => {
+    const listItem = createNewTaskElement(inputTsk.value);
+    doingTsk.appendChild(listItem);
+    bindTaskEvents(listItem, taskCompleted);
+
+    inputTsk.value = "";
+}
+const editTask = function () {
+    console.log("Edit Task...");
+
+    const listItem = this.parentNode;
+
+    const editInput = listItem.querySelector("input[type=text]")
+    const label = listItem.querySelector("label");
+
+    const containsClass = listItem.classList.contains("editMode");
+    if (containsClass) {
+        label.innerText = editInput.value;
+    } else {
+        editInput.value = label.innerText;
     }
-    /*tab = [];
-    for (i = 0; i < select.options.length; i++) {
-        if (select.options.i.selected === true) { tab[i] = i }
-    }*/
-})
+    listItem.classList.toggle("editMode");
+
+}
+
+const deleteTask = function () {
+    console.log("Delete task...");
+    const listItem = this.parentNode;
+    const ul = listItem.parentNode;
+    ul.removeChild(listItem);
+}
+
+const taskCompleted = function () {
+    const listItem = this.parentNode;
+    donaTsk.appendChild(listItem);
+    bindTaskEvents(listItem, taskIncomplete);
+}
+const taskIncomplete = function () {
+    const listItem = this.parentNode;
+    doingTsk.appendChild(listItem);
+    bindTaskEvents(listItem, taskCompleted);
+}
+
+const bindTaskEvents = function (taskListItem, checkBoxEventHandler) {
+    const checkBox = taskListItem.querySelector("input[type=checkbox]");
+    const editButton = taskListItem.querySelector("button.edit");
+    const deleteButton = taskListItem.querySelector("button.delete");
+
+    editButton.onclick = editTask;
+
+    deleteButton.onclick = deleteTask;
+    checkBox.onchange = checkBoxEventHandler;
+}
+
+const ajaxRequest = function () {
+    console.log("AJAX Request");
+}
+buttonAdd.addEventListener("click", addTask);
+buttonAdd.addEventListener("click", ajaxRequest);
+for (var i = 0; i < doingTsk.children.length; i++) {
+    bindTaskEvents(doingTsk.children[i], taskCompleted);
+}
+
+for (var i = 0; i < donaTsk.children.length; i++) {
+    bindTaskEvents(donaTsk.children[i], taskIncomplete);
+}
